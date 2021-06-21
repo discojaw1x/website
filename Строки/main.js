e4cf8d3b9b6becb576d1e71aef88091831cc12b8
discojaw1x <83305341+discojaw1x@users.noreply.github.com>
2021-06-21 21:35:21 +0300

    Add files via upload

---
+++ Строки/main.js
@@ -0,0 +1,25 @@
+console.log("Some string");
+console.log('Aonther string');
+
+console.log('Some "long" string');
+console.log("Some 'long'string");
+
+console.log("hello there".length);
+
+console.log("Another \" very long \\ string \"");
+
+var string = "Sometimes the same is different";
+console.log(string.length);
+console.log(string.charAt(4));
+console.log(string.substring(10,21));
+console.log(string.substring(10,21));
+console.log(string.slice(-10));
+console.log(string.substr(14,4));
+console.log(string.lastIndexOf("me"));
+console.log(string.lastIndexOf("me"));
+string.replace("is", "is not");
+console.log(string.split(" "));
+console.log(string.toUpperCase());
+console.log(string.toLowerCase());
+
+console.log(string[4]);
