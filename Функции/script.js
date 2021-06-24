0586cf74ed2223b937fcfc9693de360d8452069e
discojaw1x <83305341+discojaw1x@users.noreply.github.com>
2021-06-25 01:35:40 +0300

    Add files via upload

---
+++ Функции/script.js
@@ -0,0 +1,36 @@
+//    function идентификатор(аргументы) {
+//	инструкции
+//	return выражение
+// }
+//  function greet(name) {
+// 	return "Hello " + name;
+//  }
+// var greet = function (name) {
+// 	console.log(arguments.length);
+// 	return "Hello " + name;
+// };
+
+// // выражение(аргументы)
+
+// console.log(greet("Sorax", 34,23,26).toUpperCase());
+
+// var func = function(callback) {
+// var name ="Sorax";
+// callback(name);
+// };
+// func(function(n){
+// console.log("Hello " + n);
+// });
+
+// var func = function() {
+// 	return function() {
+// 		console.log("Hi");
+// 	}
+// };
+// func()();
+
+var greeting = (function(name) {
+	return "Hello " + name;
+}("Sorax"));
+
+console.log(greeting);
