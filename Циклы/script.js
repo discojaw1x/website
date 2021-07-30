86e4146892b18d85223af746944cd1777a80de05
discojaw1x <83305341+discojaw1x@users.noreply.github.com>
2021-07-31 01:57:30 +0300

    Add files via upload

---
+++ Циклы/script.js
@@ -0,0 +1,20 @@
+// for (инициализация; тест; инкремент) тело цикла
+
+// var i;
+//  for (var i = 0; i <= 10; i++) {
+//	console.log(i);
+// }
+// for (i=10; i > 0; i--) {
+//	console.log(i);
+// }
+
+// while (выражение) инструкция
+
+// var i = 10;
+// while (i--) {
+// 	console.log(i);
+// }
+
+// do инструкция while (выражение)
+var i = 0;
+do console.log(i++); while (i<10)
